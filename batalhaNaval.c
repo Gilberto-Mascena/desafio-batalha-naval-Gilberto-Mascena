13ad676c3b830825ecff816856db067778612e63
Gilberto Mascena <gilbertomascena@gmail.com>
2025-04-03 18:26:46 -0300

    :sparkles: feat: implemented naval battle challenge

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,11 +4,89 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+int main()
+{
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    int tabuleiro[10][10] = {0}; // Inicializa o tabuleiro com 0 (vazio)
+
+    /*
+     Atribuíção de valores manualmente para representar os navios no tabuleiro.
+     Exemplo de posicionamento de navios:
+     Navio 1 (horizontal)
+
+     tabuleiro[4][3] = 3; // Parte do navio
+     tabuleiro[4][4] = 3; // Parte do navio
+     tabuleiro[4][5] = 3; // Parte do navio
+     */
+
+    // Laços aninhados para preencher o tabuleiro com navios
+    // Navio 1 (horizontal)
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i == 0 && j == 0)
+            {
+                tabuleiro[4][3] = 3; // Parte do navio
+            }
+            else if (i == 0 && j == 1)
+            {
+                tabuleiro[4][4] = 3; // Parte do navio
+            }
+            else if (i == 0 && j == 2)
+            {
+                tabuleiro[4][5] = 3; // Parte do navio
+            }
+        }
+    }
+
+    /*
+    Exemplo de atribuição manual para o segundo navio (vertical)
+    Navio 2 (vertical)
+
+    tabuleiro[6][4] = 3; // Parte do navio
+    tabuleiro[7][4] = 3; // Parte do navio
+    tabuleiro[8][4] = 3; // Parte do navio
+    */
+
+    // Exemplo de preenchimento do tabuleiro com laços aninhados
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i == 6 && j == 4)
+            {
+                tabuleiro[i][j] = 3; // Parte do navio
+            }
+            else if (i == 7 && j == 4)
+            {
+                tabuleiro[i][j] = 3; // Parte do navio
+            }
+            else if (i == 8 && j == 4)
+            {
+                tabuleiro[i][j] = 3; // Parte do navio
+            }
+        }
+    }
+    // Exibe o tabuleiro
+    printf("Tabuleiro:\n");
+    printf("\n");
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    // Exibe as coordenadas dos navios
+    printf("\nCoordenadas dos Navios:\n");
+    printf("Navio 1: (4,3) (4,4) (4,5)\n");
+    printf("Navio 2: (6,4) (7,4) (8,4)\n");
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -25,7 +103,7 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
-    
+
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
